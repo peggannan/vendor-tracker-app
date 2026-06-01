356253a47d12672355e2384e7e330be0abda4d2c
bernard-bfl <aggreybernard3@gmail.com>
2026-06-01 04:56:27 -0400

    Delete backend directory

--- backend/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from .views import ProductListCreateView, ProductDetailView, RestockView
-
-
-urlpatterns = [
-    path('', ProductListCreateView.as_view(), name='product-list-create'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('<int:pk>/restock/', RestockView.as_view(), name='product-restock'),
-]
