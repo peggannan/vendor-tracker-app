763efe74e2e727caf394c9f21dbd983a85cad634
berNard <aggreybernard3@gmail.com>
2026-06-04 15:55:27 +0000

    allowed hosts config

--- backend/vendortracker/settings.py
+++ backend/vendortracker/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'vendor-tracker-app.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
